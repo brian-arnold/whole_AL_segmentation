5e85d3e255da33a89dc3d3d921c63f527aa49ada
Brian J. Arnold <bjarnold@scotty.pni.princeton.edu>
2024-07-02 12:24:44 -0400

    tests passed!

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -6,16 +6,36 @@ import pytest
 import sys
 import os
 import glob
-import caiman as cm
 import pickle
 import pandas as pd
 import numpy as np
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-
 from experiment_info import samples, data_dir, puffs, params
-import functions as fn
 
+import tmp_activity_traces as activity_traces
+import tmp_video_IO as video_IO
+import tmp_binary_mask as bm
+
+
+def convert_to_df(maxs, name, puffs):
+    # df_list = []
+
+    df = pd.DataFrame.from_dict(maxs)
+    df['samp'] = name
+    df['subpop'] = name.split('_')[1]
+    df['trial'] = df.index+1
+
+    df = df.reset_index(drop=True)
+    df = pd.melt(df, id_vars=['samp', 'subpop', 'trial'], var_name='odor', value_name='value')
+
+    odor_order = {}
+    for puff in puffs:
+        if puff.trial == 1:
+            odor_order[puff.odor_name] = puff.number
+
+    df['odor_order'] = df['odor'].map(odor_order)
+    return df
 
 #######
 # Some functions require unnormalized data, others normalized data, 
@@ -24,16 +44,16 @@ import functions as fn
 
 @pytest.fixture
 def vids_unnormalized():
-    vids = glob.glob(f"{data_dir}/{samples[0]}/*.registered.tif")
-    vids = sorted(vids)
-    vid_list = fn.load_videos_into_list(vids, params, normalize=False)
+    vid_fnames = glob.glob(f"{data_dir}/{samples[0]}/*.registered.tif")
+    vid_fnames = sorted(vid_fnames)
+    vid_list = video_IO.load_videos(vid_fnames, params, normalize=False)
     return vid_list
 
 @pytest.fixture
 def vids_normalized():
-    vids = glob.glob(f"{data_dir}/{samples[0]}/*.registered.tif")
-    vids = sorted(vids)
-    vid_list = fn.load_videos_into_list(vids, params, normalize=True)
+    vid_fnames = glob.glob(f"{data_dir}/{samples[0]}/*.registered.tif")
+    vid_fnames = sorted(vid_fnames)
+    vid_list = video_IO.load_videos(vid_fnames, params, normalize=True)
     return vid_list
 
 #######
@@ -43,8 +63,7 @@ def vids_normalized():
 @pytest.fixture
 def test_binary_mask(vids_unnormalized):
     # the binary mask function performs median filtering, so give it unnormalized data
-    Y = cm.concatenate(vids_unnormalized)
-    binary_mask = fn.find_binary_mask(Y)
+    binary_mask, binary_mask_frac = bm.find_binary_mask(vids_unnormalized, params)
     with open('tests/data/binary_masks_sample0.pkl', 'rb') as f:
         binary_mask_dict = pickle.load(f)
         binary_mask_expected = binary_mask_dict[samples[0]]
@@ -53,24 +72,21 @@ def test_binary_mask(vids_unnormalized):
 
 @pytest.fixture
 def test_mean_activity(vids_normalized, test_binary_mask):
-    Y = cm.concatenate(vids_normalized)
     mean_activity_within_segment = {}
-    mean_activity_within_segment[samples[0]] = fn.extract_mean_activity_within_binary_mask(Y, test_binary_mask, params)
+    mean_activity_within_segment = bm.mean_activity_within_binary_mask(vids_normalized, test_binary_mask, params)
     df = pd.read_csv('tests/data/mean_activity_within_segment_sample0.csv')
     mean_activity_expected = np.array(df.iloc[:,0])
     # assert that the first column of df_expected is the same as mean_activity_within_segment
-    assert np.allclose(mean_activity_within_segment[samples[0]], mean_activity_expected)
+    assert np.allclose(mean_activity_within_segment, mean_activity_expected)
     return mean_activity_within_segment
 
 def test_max_response(test_mean_activity):
-    max_responses, _ = fn.compute_max_responses(test_mean_activity, puffs, params['n_frames_to_analyze'])
-    max_responses_df = fn.convert_to_df(max_responses, puffs)
+    max_responses, _ = activity_traces.max_activity_per_odor(test_mean_activity, puffs, params)
+
+    max_responses_df = convert_to_df(max_responses, samples[0], puffs)
     observed = max_responses_df.value
 
     expected_df = pd.read_csv('tests/data/peak_max_df_sample0.csv')
     expected = np.array(expected_df.value)
     # assert that the first column of df_expected is the same as max_responses
     assert np.allclose(observed, expected)
-
-
-
